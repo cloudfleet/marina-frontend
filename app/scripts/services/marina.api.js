36c14efa0820e03c83a50eab1579ce0f4b9620f0
Christoph Witzany <christoph@web.crofting.com>
2015-08-03 22:05:48 +0200

    add fetch repo list to api service

--- app/scripts/services/marina.api.js
+++ app/scripts/services/marina.api.js
@@ -11,89 +11,23 @@ angular.module('marinaFrontendApp')
   .factory('marinaApi', ['$resource', '$http', '$q', '$rootScope', '$state', '$localstorage',
     function ($resource, $http, $q, $rootScope, $state, $localstorage) {
 
-      var localStorageUserKey = 'cloudfleet.cockpit.currentUser';
-
-      var storeCurrentUser = function(user)
-      {
-        $localstorage.setObject(localStorageUserKey, user);
-      };
-
-      var  clearCurrentUser = function()
-      {
-        $localstorage.getObject(localStorageUserKey, null);
-      };
+      var localStorageBuildlogsKeyPrefix = 'marina.builds.logs.';
 
       var service = {
 
-        getCurrentUser: function()
-        {
-          return $localstorage.getObject(localStorageUserKey);
-        },
-
-        login: function (username, password) {
-
-          console.log('logging in user ' + username)
+        getRepositories: function () {
 
           var deferred = $q.defer();
 
-          $http.post('/musterroll/login', {
-            'username': username,
-            'password': password
-          }).
-            success(function () {
-
-              $http.get('/musterroll/api/v1/currentUser').
-                success(function(data){
-                  storeCurrentUser(data);
-                  deferred.resolve(data);
-                }).
-                error(function () {
-                  deferred.resolve(false);
-
-                });
+          $http.get('/api/v1/repos/').
+            success(function (data) {
+              deferred.resolve(data.repositories);
             }).
             error(function () {
-              deferred.resolve(false);
-
-            });
-          return deferred.promise;
-        },
-
-        logOut: function(){
-          var deferred = $q.defer();
-          $http.get('/musterroll/logout').
-            success(function (data) {
-              clearCurrentUser();
-              deferred.resolve(data);
-
-            }).
-            error(function (_, status) {
-
-              deferred.resolve(status);
-            });
-
-          return deferred.promise;
-        },
-
-        loadCurrentUser: function () {
-          var deferred = $q.defer();
-
-          deferred.resolve(status);
-          $http.get('/musterroll/api/v1/currentUser').
-            success(function (data) {
-              storeCurrentUser(data);
-              deferred.resolve(data);
-            }).
-            error(function (_, status) {
-              clearCurrentUser();
               deferred.resolve(null);
             });
-
           return deferred.promise;
-        }
-
-
-
+        },
       };
       return service;
 
